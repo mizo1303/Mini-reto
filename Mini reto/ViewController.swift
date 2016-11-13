c98f06165af723fdeb809e2652c360d5ae7fc45f
Luis Miguel Zozaya Martinez <LuisMiguel@MacBook-Pro-de-Luis-2.local>
2016-11-12 23:15:29 -0600

    Initial Commit

---
+++ Mini reto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mini reto
+//
+//  Created by Luis Miguel Zozaya Martinez on 12/11/16.
+//  Copyright © 2016 Luis Miguel Zozaya Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
